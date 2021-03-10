2038ddbe3470ad146395cbbaa29afc9100289af4
Victor Hazin da Rocha <victorhazin@gmail.com>
2021-03-10 10:19:25 -0300

    aula sobre layouts
    aula sobre layouts

--- HelloWorld/app/src/main/java/com/cesarschool/helloword/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/cesarschool/helloword/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.cesarschool.helloword
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,15 +8,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var count = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         buttonMainActivity.setOnClickListener{
-            mainActivityTextView.setText("O Texto mudou"+ count++)
-            Log.d("Button", "cliquei")
+            val novaActivity = Intent(this, NovaActivity::class.java)
+            startActivity(novaActivity)
         }
 
     }
